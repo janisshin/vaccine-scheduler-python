73084c3d5bcf0ae59eb965cdf46609164661965d
Janis Shin <49432660+janisshin@users.noreply.github.com>
2022-09-06 17:50:42 -0700

    Add files via upload

--- src/main/resources/create.sql
+++ src/main/resources/create.sql
@@ -1,18 +1,34 @@
-CREATE TABLE Caregivers (
-    Username varchar(255),
-    Salt BINARY(16),
-    Hash BINARY(16),
-    PRIMARY KEY (Username)
+CREATE TABLE Insurance (
+  InsurerID BIGINT PRIMARY KEY,
+  CompanyName VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Availabilities (
-    Time date,
-    Username varchar(255) REFERENCES Caregivers,
-    PRIMARY KEY (Time, Username)
+CREATE TABLE Patient (
+  PatientID BIGINT PRIMARY KEY,
+  PatientName VARCHAR(255) NOT NULL,
+  ZipCode INT NOT NULL,
+  IID BIGINT REFERENCES Insurance,
+  InsurancePolicy VARCHAR
 );
 
-CREATE TABLE Vaccines (
-    Name varchar(255),
-    Doses int,
-    PRIMARY KEY (Name)
+CREATE TABLE Clinic (
+  ClinicID BIGINT PRIMARY KEY,
+  ClinicName VARCHAR(255) NOT NULL,
+  Address VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Visits (
+  VisitID BIGINT PRIMARY KEY,
+  PID BIGINT REFERENCES Patient NOT NULL,
+  CID BIGINT REFERENCES Clinic NOT NULL,
+  Reason VARCHAR(255) NOT NULL,
+  VisitTime DATE NOT NULL,
+  Cost MONEY NOT NULL,
+  Debt MONEY NOT NULL
+);
+
+CREATE TABLE AcceptedBy (
+  IID BIGINT REFERENCES Insurance,
+  CID BIGINT REFERENCES Clinic,
+  PRIMARY KEY (IID, CID)
 );
